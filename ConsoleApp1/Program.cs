0b840e2fe694e41b1689a7ef0e9a6dc89ae8c7de
CodeBootlegger <brozzers666@gmail.com>
2018-06-13 12:41:58 +0300

    Finishh

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.Social;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocialSpammer socialSpammer = new SocialSpammer(new Facebook());
+            socialSpammer.Send();
+        }
+    }
+}
